fdfeebbe9fe08c37d7b085511857ea6d1d011bd9
亮 <liang.wang@wormpex.com>
2018-07-25 23:36:26 +0800

    add

--- app/src/main/java/changxianghudong/com/salary_android/dao/CityDao.java
+++ app/src/main/java/changxianghudong/com/salary_android/dao/CityDao.java
@@ -14,6 +14,10 @@ public class CityDao {
 
     private Context context;
 
+    /**
+     * 查询所有热门城市
+     * @return
+     */
     public List<City> getAllHotCity() {
         List<City> hotCity = Lists.newArrayList();
         DBHelper dbHelper = new DBHelper(this.context);
@@ -21,14 +25,35 @@ public class CityDao {
         String sql  = "select * from City where is_hot = ?";
         Cursor cursor = sqLiteDatabase.rawQuery(sql, new String[]{"1"});
         while (cursor.moveToNext()) {
-            City city = new City();
-            city.setId(cursor.getInt(0));
-            city.setCityName(cursor.getString(2));
-            city.setCityInitial(cursor.getString(3));
-            city.setIsHot(cursor.getInt(4));
-            hotCity.add(city);
+            buildCity(hotCity, cursor);
         }
         return hotCity;
+    }
+
+    /**
+     * 查询所有城市
+     *
+     * @return
+     */
+    public List<City> getAllCity() {
+        List<City> hotCity = Lists.newArrayList();
+        DBHelper dbHelper = new DBHelper(this.context);
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        String sql  = "select * from City";
+        Cursor cursor = sqLiteDatabase.rawQuery(sql, new String[]{"1"});
+        while (cursor.moveToNext()) {
+            buildCity(hotCity, cursor);
+        }
+        return hotCity;
+    }
+
+    private void buildCity(List<City> hotCity, Cursor cursor) {
+        City city = new City();
+        city.setId(cursor.getInt(0));
+        city.setCityName(cursor.getString(2));
+        city.setCityInitial(cursor.getString(3));
+        city.setIsHot(cursor.getInt(4));
+        hotCity.add(city);
     }
 
 }
