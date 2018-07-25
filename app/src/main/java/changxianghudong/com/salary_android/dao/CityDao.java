e9ea87dbc81eadbf0589cf688279fe437bae3b21
亮 <liang.wang@wormpex.com>
2018-07-25 09:33:39 +0800

    add

--- app/src/main/java/changxianghudong/com/salary_android/dao/CityDao.java
+++ app/src/main/java/changxianghudong/com/salary_android/dao/CityDao.java
@@ -1,8 +1,34 @@
 package changxianghudong.com.salary_android.dao;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+import changxianghudong.com.salary_android.model.City;
 
 public class CityDao {
 
-    private SQLiteDatabase sqLiteDatabase;
+    private Context context;
+
+    public List<City> getAllHotCity() {
+        List<City> hotCity = Lists.newArrayList();
+        DBHelper dbHelper = new DBHelper(this.context);
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        String sql  = "select * from City where is_hot = ?";
+        Cursor cursor = sqLiteDatabase.rawQuery(sql, new String[]{"1"});
+        while (cursor.moveToNext()) {
+            City city = new City();
+            city.setId(cursor.getInt(0));
+            city.setCityName(cursor.getString(2));
+            city.setCityInitial(cursor.getString(3));
+            city.setIsHot(cursor.getInt(4));
+            hotCity.add(city);
+        }
+        return hotCity;
+    }
+
 }
---
+++ app/src/main/java/changxianghudong/com/salary_android/model/City.java
@@ -0,0 +1,44 @@
+package changxianghudong.com.salary_android.model;
+
+public class City {
+
+    private int id;
+
+    private String cityName;
+
+    private int isHot;
+
+    private String cityInitial;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getIsHot() {
+        return isHot;
+    }
+
+    public void setIsHot(int isHot) {
+        this.isHot = isHot;
+    }
+
+    public String getCityInitial() {
+        return cityInitial;
+    }
+
+    public void setCityInitial(String cityInitial) {
+        this.cityInitial = cityInitial;
+    }
+}
