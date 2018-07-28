177739c4e13f254cd962f8f5bbac5a65a74cfa5f
亮 <liang.wang@wormpex.com>
2018-07-28 13:13:01 +0800

    add

--- app/src/main/java/changxianghudong/com/salary_android/MainActivity.java
+++ app/src/main/java/changxianghudong/com/salary_android/MainActivity.java
@@ -1,10 +1,12 @@
 package changxianghudong.com.salary_android;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        EditText editText = this.findViewById(R.id.salaryText);
+        editText.setBackgroundColor(Color.RED);
     }
 
     @Override
